91cc1a5ad52df231ff233eb536886ee5fabc2ca3
HuitaePark <qkrgmlxo3174@gmail.com>
2025-06-17 16:32:10 +0900

    feat : 마이페이지 경로 추가

--- src/main/java/com/mujakjung/page/PageController.java
+++ src/main/java/com/mujakjung/page/PageController.java
@@ -9,21 +9,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
     @Value("${kakao.map.Key}")
     private String key;
-
+    //메인페이지
     @GetMapping("/")
     public String mujakjung(Model model){
         model.addAttribute("appKey",key);
         return "mujakjung";
     }
-
+    //회원가입 성공시 로그인페이지로 이동
     @GetMapping("/join-success")
     public String joinSuccess(){
         return "login";
     }
-
+    //회원가입 페이지
     @GetMapping("/join-page")
     public String joinProc(){
         return "join";
+    }
+    //마이페이지
+    @GetMapping("/myPage")
+    public String myPage(){
+        return "myPage";
     }
 
 }
