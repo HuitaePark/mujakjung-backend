6f7ed1ac221fc6cd53167edf0f5b52b89be7309f
HuitaePark <qkrgmlxo3174@gmail.com>
2025-06-23 03:13:03 +0900

    feat : 에러상황 발생시 응답할수 있는 에러 리스폰스 생성

--- src/main/java/com/mujakjung/global/error/ErrorResponse.java
+++ src/main/java/com/mujakjung/global/error/ErrorResponse.java
@@ -8,5 +8,26 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ErrorResponse {
 
+    private String message;
+    private String code;
+
+    private ErrorResponse(final ErrorCode code) {
+        this.message = code.getMessage();
+        this.code = code.getCode();
+    }
+
+    // 필요에 따라 에러 메시지를 직접 설정할 수 있도록 오버로딩
+    public ErrorResponse(final ErrorCode code, final String message) {
+        this.message = message;
+        this.code = code.getCode();
+    }
+
+    public static ErrorResponse of(final ErrorCode code) {
+        return new ErrorResponse(code);
+    }
+
+    public static ErrorResponse of(final ErrorCode code, final String message) {
+        return new ErrorResponse(code, message);
+    }
 }
 
